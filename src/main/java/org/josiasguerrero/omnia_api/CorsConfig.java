610578f5af534e0c32d3640bdbef251fbe7b1ec0
Josias <josiasguerrero2004@gmail.com>
2025-12-31 18:40:36 -0600

    feat: migration and deploy

--- src/main/java/org/josiasguerrero/omnia_api/CorsConfig.java
+++ src/main/java/org/josiasguerrero/omnia_api/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.josiasguerrero.omnia_api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,18 +9,21 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+
+  @Value("${cors.allowed.origin}")
+  private String allowedOrigin;
+
   @Bean
   public CorsFilter corsFilter() {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
 
     config.setAllowCredentials(true);
-    config.addAllowedOrigin("http://localhost:3000");
+    config.addAllowedOrigin(allowedOrigin); // ← aquí ya usa la variable
     config.addAllowedHeader("*");
     config.addAllowedMethod("*");
 
     source.registerCorsConfiguration("/api/**", config);
     return new CorsFilter(source);
   }
-
 }
